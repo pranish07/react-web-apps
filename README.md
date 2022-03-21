82bdf531bdd9e1b5370c31d0660921954721bd0c
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-03-21 09:08:34 +0545

    Deploy React app to GitHub Pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# This app works with providing the following apps by routing.
-1. useful websites
-2. dice game
--- src/apps/usefulwebsite/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button(props) {
-  return (
-    <a href={props.link} target="_blank"><button className='uw-button'>{props.name}</button></a>
-  )
-}
-
-export default Button;
